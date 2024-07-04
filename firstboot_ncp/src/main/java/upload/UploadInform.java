90e31ffb4421e291bcecec14c13a71cbfa828568
Valencia Miyeon Jang <miyeon5230@gmail.com>
2024-07-04 10:59:15 +0900

    Modified the 'UploadInform.java' file.

--- firstboot_ncp/src/main/java/upload/UploadInform.java
+++ firstboot_ncp/src/main/java/upload/UploadInform.java
@@ -1,6 +1,9 @@
 package upload;
 
 public class UploadInform {
-	public static final String uploadPath = "c://fullstack/upload/";
-	public static final String uploadLocation = "file:///c:/fullstack/upload/";
+	//public static final String uploadPath = "c://fullstack/upload/";
+	//public static final String uploadLocation = "file:///c:/fullstack/upload/";
+	
+	public static final String uploadPath = "/usr/mydir/upload";
+	public static final String uploadLocation = "file:///usr/mydir/upload/";
 }
